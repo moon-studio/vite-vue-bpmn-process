7f004a76f4c0592f18b556c6f46a7f88e7525a67
MiyueSC <913784771@qq.com>
2022-05-17 18:04:40 +0800

    Update global.d.ts

--- types/declares/global.d.ts
+++ types/declares/global.d.ts
@@ -4,6 +4,9 @@ declare global {
   interface Window {
     bpmnInstances: any
   }
+  interface Object {
+    [key: string | number]: string | number | boolean | undefined | null | Object | Function
+  }
 }
 
 declare interface Window {
