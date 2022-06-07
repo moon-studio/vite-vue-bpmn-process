f11dd054d04caec0c80db3dceefce11b54b584e6
MiyueSC <913784771@qq.com>
2022-06-07 16:04:37 +0800

    feat: custom renderer 更新

--- src/components/AddiModules/Renderer/CustomRendererProvider.ts
+++ src/components/AddiModules/Renderer/CustomRendererProvider.ts
@@ -1,16 +1,11 @@
-import BpmnRenderer, { RendererHandler, RendererType } from 'bpmn-js/lib/draw/BpmnRenderer'
+import BpmnRenderer from 'bpmn-js/lib/draw/BpmnRenderer'
 import EventBus from 'diagram-js/lib/core/EventBus'
 import Styles from 'diagram-js/lib/draw/Styles'
 import PathMap from 'bpmn-js/lib/draw/PathMap'
 import Canvas from 'diagram-js/lib/core/Canvas'
 import TextRenderer from 'bpmn-js/lib/draw/TextRenderer'
-import { isObject } from 'min-dash'
-import {
-  append as svgAppend,
-  attr as svgAttr,
-  create as svgCreate,
-  classes as svgClasses
-} from 'tiny-svg'
+import renderEventContent from '@/components/AddiModules/Renderer/renderEventContent'
+import { drawCircle, drawPath } from '@/components/AddiModules/Renderer/utils'
 
 export default class CustomRendererProvider extends BpmnRenderer {
   _styles: Styles
@@ -34,40 +29,17 @@ export default class CustomRendererProvider extends BpmnRenderer {
         attrs['fill'] = '#1bbc9d'
         attrs['strokeWidth'] = 0
       }
-      return this.drawCircle(parentGfx, element.width, element.height, attrs)
+      return drawCircle(this, parentGfx, element.width, element.height, attrs)
     }
     this.handlers['bpmn:EndEvent'] = (parentGfx, element, attrs) => {
-      if (!attrs || !attrs['fillOpacity']) {
-        !attrs && (attrs = {})
-        attrs['fillOpacity'] = 1
-        attrs['fill'] = '#e98885'
-        attrs['stroke'] = '#000000'
-        attrs['strokeWidth'] = 2
-      }
-      return this.drawCircle(parentGfx, element.width, element.height, attrs)
+      const circle = this.handlers['bpmn:Event'](parentGfx, element, {
+        fillOpacity: 1,
+        strokeWidth: 2,
+        fill: '#e98885',
+        stroke: '#000000'
+      })
+      renderEventContent(this.handlers, element, parentGfx)
+      return circle
     }
-  }
-
-  private drawCircle(parentGfx, width, height, offset, attrs?) {
-    if (isObject(offset)) {
-      attrs = offset
-      offset = 0
-    }
-    offset = offset || 0
-    attrs = this._styles.computeStyle(attrs)
-    if (attrs.fill === 'none') {
-      delete attrs.fillOpacity
-    }
-    const cx = width / 2,
-      cy = height / 2
-    const circle = svgCreate('circle')
-    svgAttr(circle, {
-      cx: cx,
-      cy: cy,
-      r: Math.round((width + height) / 4 - offset)
-    })
-    svgAttr(circle, attrs)
-    svgAppend(parentGfx, circle)
-    return circle
   }
 }
